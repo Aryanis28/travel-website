c20bc9f6d98cc006e9b35976e69feb43ee0c3def
Aryanis28 <141750824+Aryanis28@users.noreply.github.com>
2024-02-16 21:22:25 +0530

    Add files via upload

---
+++ book.js
@@ -0,0 +1,55 @@
+
+    function showSeats() {
+      var bookButton = document.querySelector(".book-button");
+      bookButton.style.display = "block";
+    }
+
+    function showSeat() {
+        var bookButton = document.querySelector(".book-button");
+        bookButton.style.display = "block";
+      }
+  
+
+    
+    function america() {
+        window.location.href = "America.html";
+    }
+  
+    function swis() {
+        window.location.href = "swiss.html";
+    }
+
+    function tokyo() {
+      window.location.href = "tokyo.html";
+  }
+
+  function singa() {
+    window.location.href = "singapore.html";
+}
+
+function france() {
+  window.location.href = "france.html";
+}
+
+function italy()
+{
+  
+    window.location.href = "italy.html";
+  
+}
+function spain() {
+  window.location.href = "spain.html";
+}
+
+function london() {
+  window.location.href = "london.html";
+}
+
+function dubai() {
+  window.location.href = "dubai.html";
+}
+
+function abu() {
+  window.location.href = "abu.html";
+}
+  
